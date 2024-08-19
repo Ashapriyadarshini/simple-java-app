cbe07f633eefd9bd653f18089e43e54dfcc5eb32
asha <ashapriyadarsini16@gmail.com>
2024-08-19 20:21:27 +0530

    Initial commit

---
+++ src/main/java/com/example/simple_java_app/App.java
@@ -0,0 +1,15 @@
+package com.example.simple_java_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static String getGreeting() {
+        return "Hello, CI/CD World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getGreeting());
+    }
+}
---
+++ src/test/java/com/example/simple_java_app/AppTest.java
@@ -0,0 +1,11 @@
+package com.example.simple_java_app;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testGetGreeting() {
+        assertEquals("Hello, CI/CD World!", App.getGreeting());
+    }
+}
